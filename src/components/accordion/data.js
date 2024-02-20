8799065d8127d07e65ae7b6b5a999d7d86ff8a75
HalimLuman <halim.luman0@gmail.com>
2024-02-20 13:20:41 +0100

    Accordion

---
+++ src/components/accordion/data.js
@@ -0,0 +1,19 @@
+const data = [
+    {
+        id: 1,
+        question: 'What is 1 + 2',
+        answer: 'According to recent studies 1 + 2 must be equal to 3.',
+    },
+    {
+        id: 2,
+        question: 'What is 10 + 15',
+        answer: 'Recent studies suggests that the answer to the question is 25.'
+    },
+    {
+        id: 3,
+        question: 'Is 0 a real number',
+        answer: 'Eventhough 0 seems real it is just a placeholder for nothing.',
+    }
+];
+
+export default data;
---
+++ src/components/accordion/index.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import data from "./data";
+import './styles.css'
+
+export default function Accordion() {
+    const [selected, setSelected] = useState(null);
+    const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+    const [multipleSelected, setMultipleSelected] = useState([]);
+
+    function handleSingleSelection(getCurrentId) {
+        setSelected(getCurrentId === selected ? null : getCurrentId);
+    }
+
+    function handleMultipleSelection(getCurrentId) {
+        setMultipleSelected([...multipleSelected, getCurrentId]);
+        if (multipleSelected.includes(getCurrentId)) {
+            const filteredArray = multipleSelected.filter(item => item !== getCurrentId);
+            setMultipleSelected(filteredArray);
+        }       
+    }
+
+    return (
+        <div className="wrapper">
+            <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+                {enableMultiSelection ? 'Disable Multi Selection' : 'Enable Multi Selection'}
+            </button>
+            <div className="accordion">
+                {data && data.length > 0
+                    ? data.map(dataItem =>
+                        <div className="item" key={dataItem.id}>
+                            <div onClick={enableMultiSelection ? () => handleMultipleSelection(dataItem.id) : () => handleSingleSelection(dataItem.id)} className="title">
+                                <h3>{dataItem.question}</h3>
+                                <span>+</span>
+                            </div>
+                            {
+                                enableMultiSelection
+                                    ? multipleSelected.includes(dataItem.id) && (<div className="content">
+                                        {dataItem.answer}
+                                    </div>)
+                                    : selected === dataItem.id && (
+                                        <div className="content">
+                                            {dataItem.answer}
+                                        </div>
+                                    )
+                            }
+                        </div>)
+                    : <div>No data found</div>}
+            </div>
+        </div>
+    )
+}
